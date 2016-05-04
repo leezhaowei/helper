e0d8896b07de9ca04c9575c633fa40ff59a1bc40
Zhaowei Li <leezhaowei@hotmail.com>
2016-05-04 16:22:29 +0800

    add jdom

---
+++ helper/src/main/java/com/zwli/util/Xmlutils.java
@@ -0,0 +1,24 @@
+package com.zwli.util;
+
+import org.jdom2.Document;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+public class Xmlutils {
+
+    public static void outputXML(final String fn) throws Exception {
+        final SAXBuilder builder = new SAXBuilder();
+        final Document doc = builder.build(fn);
+        Format fmt = Format.getRawFormat();
+        fmt = Format.getCompactFormat();
+        final XMLOutputter out = new XMLOutputter();
+        out.setFormat(fmt);
+        out.output(doc, System.out);
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final String fn = "e:/tmp.xml";
+        Xmlutils.outputXML(fn);
+    }
+}
